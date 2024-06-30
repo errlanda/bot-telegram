78c104bf20c2145b8642a0cd3be439712b8ad6c8
Erland <73932409+errlanda@users.noreply.github.com>
2024-06-30 19:51:21 +0700

    Update Bot.ts

--- src/app/Bot.ts
+++ src/app/Bot.ts
@@ -12,7 +12,7 @@ class Bot extends TelegramBot {
             const isInCommand: boolean = Object.values(commands).some((keyword: RegExp) => keyword.test(data.text))
 
             if (!isInCommand) {
-                const result: string = "ketik !help atau klik tombol dibawah ini untuk memunculkan panduan"
+                const result: string = "Saya tidak mengerti 🙏\nketik !help atau klik tombol dibawah ini untuk memunculkan panduan"
 
                 this.sendMessage(id, result, replyWithButton({ text: "Panduan Penggunaan", callback_data: "go_to_help" }))
             }
