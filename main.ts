50b6482db2f667230a875906b4ff6676645b5300
Erland <erlaanda@gmail.com>
2024-06-30 19:09:28 +0700

    1

---
+++ main.ts
@@ -0,0 +1,43 @@
+import Bot from "./src/app/Bot";
+
+process.env.TZ = "Indonesia/Jakarta";
+
+require("dotenv").config();
+const result = require("dotenv").config();
+if (result.error) {
+  console.error("Error loading .env file:", result.error);
+} else {
+  console.log(".env file loaded successfully");
+}
+
+
+
+type BotOptions = {
+  polling: boolean;
+  filePath?: boolean;
+};
+
+// ...
+
+const token: string | undefined = process.env.TOKEN;
+const botOptions: BotOptions = { polling: true, filePath: false };
+
+const bot = new Bot(token, botOptions);
+
+function main(): void {
+  bot.showHelp();
+  bot.showFollowedText();
+  bot.showAvatar();
+  bot.showEarthQuakeInfo();
+  bot.showGreeting();
+  bot.showNews();
+  bot.showQuotes();
+}
+
+console.log("Bot is running now!");
+console.log("Token:", process.env.TOKEN);
+
+
+
+main();
+
---
+++ src/libs/constants.ts
@@ -0,0 +1,21 @@
+const helpText: string = ` 🍻 Panduan Penggunaan 🍻
+
+👇 Basic Command 👇
+---
+    !help memunculkan bantuan
+    !quotes memunculkan quotes secara acak
+    !gempa info gempa terbaru dari BMKG
+    !halo cek profile kamu
+    !news menampilkan berita terbaru
+---
+
+👇 Command with parameter 👇
+---
+    !avatar [nama] generate gambar avatar buatmu
+    !follow [ucapan] mengikuti apa ucapanmu 
+---
+`
+
+const commandPrefix: string = "!";
+
+export { helpText, commandPrefix }
